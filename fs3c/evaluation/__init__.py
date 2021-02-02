304508f9e7daaa0fc89752aebabdeea2dd752d5e
Anqw <41106580+Anqw@users.noreply.github.com>
2021-02-02 11:28:37 +0800

    Update __init__.py

--- fs3c/evaluation/__init__.py
+++ fs3c/evaluation/__init__.py
@@ -1,6 +1,7 @@
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
 from .coco_evaluation import COCOEvaluator
 from .evaluator import DatasetEvaluator, DatasetEvaluators, inference_context, inference_on_dataset
+from .lvis_evaluation import LVISEvaluator
 from .pascal_voc_evaluation import PascalVOCDetectionEvaluator
 from .testing import print_csv_format, verify_results
 
